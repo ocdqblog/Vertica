a4c743a5a2a7495fda23ee1a3137ea176d60388a
Jim Harris <101600097+ocdqblog@users.noreply.github.com>
2022-03-28 08:06:10 -0500

    Delete Demo-BRS-04-01_Select_BRS_2016_2021_Batting_input.sql

--- Demo-BRS-04-01_Select_BRS_2016_2021_Batting_input.sql
+++
@@ -1,6 +0,0 @@
--------------------------------------------------------
--- Contents of the BRS_2016_2021_Batting_input table
---    Quick Look at the first 27 rows
--------------------------------------------------------
-
-SELECT * FROM BRS_2016_2021_Batting_input LIMIT 27;
